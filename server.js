845a66d2e95cd4b8111138ef117007c811ce998d
fuzzbdn <magnus.moreskog@gmail.com>
2026-06-04 09:09:38 +0200

    Update server.js

--- server.js
+++ server.js
@@ -14,6 +14,9 @@ const authRoutes = require('./routes/auth');
 // Säg åt servern att använda rutterna
 app.use('/api/auth', authRoutes);
 
+const discogsRoutes = require('./routes/discogs');
+app.use('/api/discogs', discogsRoutes);
+
 // Den vanliga välkomst-rutten
 app.get('/', (req, res) => {
     res.json({ message: 'Välkommen till Tradeogs API! Servern är live.' });
