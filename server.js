f0ad532c945fe49ea228f1db5e792ebe0e4af024
fuzzbdn <magnus.moreskog@gmail.com>
2026-06-04 20:48:53 +0200

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,9 @@ app.use(express.json());
 
 // LÄGG TILL DENNA FÖR VERCEL (så den hittar index.html)
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.get('/api/ping', (req, res) => {
+    res.send('PONG - NYA KODEN ÄR LIVE!');
+});
 // 1. Importera alla dina rutter från routes-mappen
 const authRoutes = require('./routes/auth');
 const discogsRoutes = require('./routes/discogs');
